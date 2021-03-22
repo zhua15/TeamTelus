47486cd66ef19b0c5bfc115a4cdbc92522bcf0df
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2021-03-22 10:57:30 -0400

    add video splicer

---
+++ splicer.py
@@ -0,0 +1,29 @@
+from cv2 import cv2
+import numpy as np
+import os
+
+# Playing video from file:
+cap = cv2.VideoCapture('test.mp4') # change this to video in question 
+
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print ('Error: Creating directory of data')
+
+currentFrame = 0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Saves image of the current frame in jpg file
+    name = './data/frame' + str(currentFrame) + '.jpg'
+    print ('Creating...' + name)
+    cv2.imwrite(name, frame)
+
+    # To stop duplicate images
+    currentFrame += 3 # jumps 3 frames to give about one frame per 8 milliseconds
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
